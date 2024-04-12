3a69b238a5023d5da46076eba7c9aab9c8b66ab1
sotywill <sotonyewilliam@yahoo.com>
2024-04-11 19:59:34 -0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: SOTONYE WILLIAM-TAMUNOTONYE Student ID: 117398222 Date: 11/04/2024
+*
+*  Online (Cyclic) Link: https://angry-fly-singlet.cyclic.app
+*
+********************************************************************************/
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
